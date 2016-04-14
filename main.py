53097b85b9194e9b4ab016d0bdc6965fb91098a3
Eddy Hintze <eddy.hintze@gmail.com>
2016-04-14 11:23:56 -0400

    Added Host config. Added realtime scraper data back for now

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def before_first_request():
     db.session.commit()
 
 if __name__ == '__main__':
-    socketio.run(app, port=app.config.get('PORT'), debug=app.config.get('DEBUG'))
+    socketio.run(app, host=app.config.get('HOST'), port=app.config.get('PORT'), debug=app.config.get('DEBUG'))
