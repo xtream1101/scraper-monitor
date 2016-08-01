df3940839987fcb8c1aff36bc64e334115db007a
Eddy Hintze <eddy.hintze@gmail.com>
2016-08-01 11:49:42 -0400

    Added support to only allow certain email domains to register

--- auth.py
+++ auth.py
@@ -1,9 +1,13 @@
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
 from flask_security import Security, user_registered, SQLAlchemyUserDatastore
-from flask_security.forms import ConfirmRegisterForm, StringField, Required, unique_user_email
+from flask_security.forms import unique_user_email, RegisterForm
 
 from app import app, db
 from models import User, Role, Organization, Group
 
+from pprint import pprint
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 
 # Setup Flask-Security
@@ -11,12 +15,38 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 unique_user_attribute = unique_user_email
 
 
-class ExtendedRegisterForm(ConfirmRegisterForm):
-    first_name = StringField('First Name', [Required()])
-    last_name = StringField('Last Name', [Required()])
-    username = StringField('Username', [Required(), unique_user_attribute])
+class ExtendedRegisterForm(RegisterForm):
+    first_name = StringField('First Name', [DataRequired()])
+    last_name = StringField('Last Name', [DataRequired()])
+    username = StringField('Username', [DataRequired(), unique_user_attribute])
+    email = StringField('Email', [DataRequired(), unique_user_email])
+
+    def validate(self):
+
+        # Need this for the checks to work
+        Form.validate(self)
+
+        vaild_emails = app.config.get('SECURITY_REGISTERABLE_EMAILS')
+        pprint(self.email.data)
+        try:
+            # This will trigger an IndexError, thats fine since an email has to have an `@`
+            email_domain = self.email.data.split('@')[1].strip()
+            # Dont check email domain if there is nothign to check against
+            if email_domain != '' and email_domain not in vaild_emails and\
+               vaild_emails is not None and len(vaild_emails) > 0:
+                raise(ValueError)
+
+        except (IndexError, ValueError):
+            self.email.errors.append("Not a vaild email for this server")
+            return False
+
 
 security = Security(app, user_datastore, confirm_register_form=ExtendedRegisterForm)
+
+
+def check_email_domain(thing):
+    from pprint import pprint
+    pprint(thing)
 
 
 @user_registered.connect_via(app)
