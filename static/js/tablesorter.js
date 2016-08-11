ebb8ac80ba7ef9a36adb817358cb660211f43f03
Eddy Hintze <eddy.hintze@gmail.com>
2016-08-11 13:28:56 -0400

    Added search box for scraper run logs

--- static/js/tablesorter.js
+++ static/js/tablesorter.js
@@ -8,7 +8,7 @@ $(function(){
             // external filter (column specific or any match)
             filter_external : '.search',
             // add a default type search to the first name column
-            filter_defaultFilter: { 1 : '~{query}' },
+            // filter_defaultFilter: { 1 : '~{query}' },
             filter_columnFilters: false,
             filter_placeholder: { search : 'Search...' },
             filter_saveFilters : true,
@@ -35,8 +35,6 @@ $(function(){
             // Set to use a jQuery selector (or jQuery object) pointing to the
             // external filter (column specific or any match)
             filter_external : '.log-search',
-            // add a default type search to the first name column
-            filter_defaultFilter: { 1 : '~{query}' },
             filter_columnFilters: true,
             filter_placeholder: { search : 'Search...' },
             filter_saveFilters : true,
