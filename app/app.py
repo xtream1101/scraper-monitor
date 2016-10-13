a6110f7c38b8167eda76a7892075fa16a2dd5a72
Eddy Hintze <eddy.hintze@gmail.com>
2016-10-13 14:58:04 -0400

    Try a different method for https

--- app/app.py
+++ app/app.py
@@ -14,15 +14,23 @@ eventlet.monkey_patch()
 
 logger = logging.getLogger(__name__)
 
+def _force_https():
+    # my local dev is set on debug, but on AWS it's not (obviously)
+    # I don't need HTTPS on local, change this to whatever condition you want.
+    if not app.debug:
+        from flask import _request_ctx_stack
+        if _request_ctx_stack is not None:
+            reqctx = _request_ctx_stack.top
+            reqctx.url_adapter.url_scheme = 'https'
+
+
 # Create Flask application
 app = Flask(__name__)
+app.before_request(_force_https)
 app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = ['email', 'username']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_pyfile('../config.py')
 
-if app.config.get('FORCE_HTTPS') is True:
-    from flask_sslify import SSLify
-    sslify = SSLify(app)
 
 if app.config.get('SCHEMA') is None:
     app.config['SCHEMA'] = 'scraper_monitor'
